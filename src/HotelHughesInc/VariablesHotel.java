3cb4c13b3bcafeae6a8268af07c6415c79b58dd5
alexllanes1 <alexllanes026@gmail.com>
2023-06-18 10:55:37 -0600

    Actividad de Hotel en Java

---
+++ src/HotelHughesInc/VariablesHotel.java
@@ -0,0 +1,30 @@
+package HotelHughesInc;
+
+public class VariablesHotel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte pisosDelHotel = 10;
+		System.out.println(pisosDelHotel);
+
+		short habitaciones = 180;
+		System.out.println(habitaciones);
+		
+		int huespedesAnuales = 56979;
+		System.out.println(huespedesAnuales);	
+		
+		long huespedesMundiales = 345678267;
+		System.out.println(huespedesMundiales);	
+		
+		float costoHabitaciones = 4999.99f;
+		System.out.println(costoHabitaciones);	
+		
+		double costoSuit = 149999.99d;
+		System.out.println(costoSuit);	
+		
+		boolean habitacionLibre = true;
+		System.out.println(habitacionLibre);	
+	}
+
+}
